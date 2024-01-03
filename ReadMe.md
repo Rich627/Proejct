139b6ab813103d9116b90dd27238081f76d569b0
Rich627 <rich.liu0912@hotmail.com>
2024-01-03 14:33:15 +0800

    Modify notebook

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+Open ios/PoseEstimation
+Chose desinition
+
+model
+https://www.kaggle.com/models/google/movenet/frameworks/tensorFlow2/variations/singlepose-lightning/versions/4?tfhub-redirect=true
---
+++ ios/Pods/TensorFlowLiteC/Frameworks/TensorFlowLiteC.xcframework/ios-arm64/TensorFlowLiteC.framework/Headers/TensorFlowLiteC.h
@@ -0,0 +1,9 @@
+#import <TensorFlowLiteC/builtin_ops.h>
+#import <TensorFlowLiteC/c_api.h>
+#import <TensorFlowLiteC/c_api_experimental.h>
+#import <TensorFlowLiteC/c_api_types.h>
+#import <TensorFlowLiteC/common.h>
+#import <TensorFlowLiteC/profiler.h>
+#import <TensorFlowLiteC/telemetry_setting.h>
+#import <TensorFlowLiteC/types.h>
+#import <TensorFlowLiteC/xnnpack_delegate.h>
